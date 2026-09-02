654995ec9d56e9e1d7917c8ae850602749b52115
PatrickWang7941 <patrickwang7941@gmail.com>
2026-09-01 22:24:04 -0400

    Create IpaVowels.cs

---
+++ CBT/Data/IpaVowels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CBT.Data;
+
+public record IpaVowel(
+    string Symbol,
+    string Height,
+    string Backness,
+    string Roundedness
+);
